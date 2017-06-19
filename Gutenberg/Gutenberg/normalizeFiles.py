626a9b3a3027e703936116fdca1e187de3e53a22
Tania Akter <tania9309@gmail.com>
2017-06-18 17:50:41 -0700

    normalized data

--- Gutenberg/Gutenberg/normalizeFiles.py
+++ Gutenberg/Gutenberg/normalizeFiles.py
@@ -1,61 +1,82 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt; plt.rcdefaults()
+# import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from itertools import chain
 import shutil
 import os
+import subprocess
 
-m_authorList = {}
-with open('filenames.txt', 'r') as f:
-    for filename in f:
-        m_author = filename[:filename.index("_")]
-        m_authorList[m_author] = m_authorList.get(m_author, 0) +1
+# m_authorList = {}
+# with open('filenames.txt', 'r') as f:
+#     for filename in f:
+#         m_author = filename[:filename.index("_")]
+#         m_authorList[m_author] = m_authorList.get(m_author, 0) +1
 
-median = np.median(sorted(m_authorList.values(), reverse=True))
+# median = np.median(sorted(m_authorList.values(), reverse=True))
 
-print "Original # of books ", sum(m_authorList.values())
-print "Original # of authors ",len(m_authorList.keys())
+# print "Original # of books ", sum(m_authorList.values())
+# print "Original # of authors ",len(m_authorList.keys())
 
-authorList = {}
-with open('filenames.txt', 'r') as f:
-    for filename in f:
+# authorList = {}
+# with open('filenames.txt', 'r') as f:
+#     for filename in f:
+#         author = filename[:filename.index("_")]
+#         lst = authorList.get(author, [])
+#         lst.append(filename.strip())
+#         authorList[author] = lst
+
+# train = map(lambda lst: lst[:int(0.7 * len(lst))] if len(lst) >= median else [], authorList.values())
+# test = map(lambda lst: lst[int(0.7 * len(lst)):] if len(lst) >= median else [], authorList.values())
+
+# train = filter(lambda s: len(s) > 0, list(chain.from_iterable(train)))
+# test = filter(lambda s: len(s) > 0, list(chain.from_iterable(test)))
+
+# print "# total books greater than median per author ",(len(train) + len(test))
+
+# remainingAuthors = [k for k, v in m_authorList.items() if v >= median]
+
+# print "# of remaining authors having books greater than median ",len(remainingAuthors)
+
+# #for e in test:
+#     #base = "/home/ubuntu/NLP/Gutenberg/Gutenberg/"
+#  #   src = 'txt/' + e
+#   #  dst = 'normalizedTest/' + e
+#    # shutil.copyfile(src, dst)
+
+# #for e in train:
+#     #base = "/home/ubuntu/NLP/Gutenberg/Gutenberg/"
+#  #   src = 'txt/' + e
+#   #  dst = 'normalizedTrain/' + e
+#    # shutil.copyfile(src, dst)
+
+def normalizeContent(input_dir):
+    for filename in os.listdir(input_dir):
         author = filename[:filename.index("_")]
-        lst = authorList.get(author, [])
-        lst.append(filename.strip())
-        authorList[author] = lst
+        subprocess.call(["sed", "-i", 's/'+author+'//I', input_dir+'/'+filename])
 
-train = map(lambda lst: lst[:int(0.7 * len(lst))] if len(lst) >= median else [], authorList.values())
-test = map(lambda lst: lst[int(0.7 * len(lst)):] if len(lst) >= median else [], authorList.values())
 
-train = filter(lambda s: len(s) > 0, list(chain.from_iterable(train)))
-test = filter(lambda s: len(s) > 0, list(chain.from_iterable(test)))
+def normalizeMore(input_dir,index):
+    for filename in os.listdir(input_dir):
+        name = filename[:filename.index("_")].split(' ')
+        if(len(name) > index):
+            author = filename[:filename.index("_")].split(' ')[index]
+            subprocess.call(["sed", "-i", 's/'+author+'//I', input_dir+'/'+filename])
+            
+normalizeContent("test")
+normalizeContent("train")
 
-print "# total books greater than median per author ",(len(train) + len(test))
+normalizeMore("test",0)
+normalizeMore("train",0)
 
-remainingAuthors = [k for k, v in m_authorList.items() if v >= median]
+normalizeMore("test",1)
+normalizeMore("train",1)
 
-print "# of remaining authors having books greater than median ",len(remainingAuthors)
+normalizeMore("test",2)
+normalizeMore("train",2)
 
-#for e in test:
-    #base = "/home/ubuntu/NLP/Gutenberg/Gutenberg/"
- #   src = 'txt/' + e
-  #  dst = 'normalizedTest/' + e
-   # shutil.copyfile(src, dst)
 
-#for e in train:
-    #base = "/home/ubuntu/NLP/Gutenberg/Gutenberg/"
- #   src = 'txt/' + e
-  #  dst = 'normalizedTrain/' + e
-   # shutil.copyfile(src, dst)
-
-def normalizeContent(input_dir, output_dir):
-	for filename in os.listdir(input_dir):
-		with open(input_dir+filename) as f:
-			author = filename[:filename.index("_")]
-			with open(output_dir_file, "w") as f1:
-				for line in f:
-					if author in line:
-						f1.write(line) 
+normalizeMore("test",3)
+normalizeMore("train",3)
